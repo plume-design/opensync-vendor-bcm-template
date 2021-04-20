f3765b205b22426fe503bdedf8d8ed21f3812b8b
Lars Kristan <lkristan@plume.com>
2021-04-20 14:42:00 +0200

    Release 2.4.2.0

--- rootfs/target/OS_BCM947622DVT_EXT/INSTALL_PREFIX/bin/wifi.sh
+++ rootfs/target/OS_BCM947622DVT_EXT/INSTALL_PREFIX/bin/wifi.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-wl -i wl0 country US
-wl -i wl1 country US
-wl -i wl2 country US
+# Use Q1/153 country code for reference board. This is a US country
+# code that supports HT160 and works on both 2.4 and 5g radios
+# It is reported as: US (Q1/153) UNITED STATES
+
+wl -i wl0 down
+wl -i wl1 down
+wl -i wl2 down
+
+wl -i wl0 country Q1/153
+wl -i wl1 country Q1/153
+wl -i wl2 country Q1/153
 
